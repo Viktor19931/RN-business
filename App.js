6b0b921d36eac6ecf1f3d0b3f49d947a89aa6488
Viktor Hordubey <viktor_hordubey@yahoo.com>
2018-07-18 14:46:04 +0300

    merged navigation and qr-code libraries

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { Navigation } from 'react-native-navigation';
 
 import LoginScreen from './src/screens/Login/Login';
+import QrScreen from './src/screens/QR-Scanner/QrScanner';
 import Tab1 from './src/screens/Tab1/Tab1';
 import Tab2 from './src/screens/Tab2/Tab2';
 import Tab3 from './src/screens/Tab3/Tab3';
 
 Navigation.registerComponent( "App.LoginScreen", () => LoginScreen );
-Navigation.registerComponent( "App.Tab1", () => Tab1 );
+Navigation.registerComponent( "App.Tab1", () => QrScreen);
 Navigation.registerComponent( "App.Tab2", () => Tab2 );
 Navigation.registerComponent( "App.Tab3", () => Tab3 );
 
--- src/screens/QR-Scanner/QrScanner.js
+++ src/screens/QR-Scanner/QrScanner.js
@@ -1,86 +1,88 @@
-// import React from 'react';
-// import {
-//     StyleSheet,
-//     Text,
-//     View,
-//     TouchableOpacity,
-//     Button
-// } from 'react-native';
-// import QRCodeScanner from 'react-native-qrcode-scanner';
-//
-// export default class App extends React.Component {
-//     state = {
-//         mode: 'view'
-//     };
-//
-//     onSuccess = (e) => {
-//         this.setState({
-//             mode: 'view'
-//         });
-//         alert('this data => ' + e.data);
-//     };
-//
-//     onOpenQRcode = () => {
-//         this.setState({
-//             mode: 'qr-code'
-//         });
-//     };
-//
-//     render() {
-//         let content = (
-//             <Button
-//                 onPress={this.onOpenQRcode}
-//                 title="Scan QR-Code"
-//                 color="#841584">
-//             </Button>
-//         );
-//
-//         if (this.state.mode === 'qr-code') {
-//             content = (
-//                 <QRCodeScanner
-//                     onRead={this.onSuccess}
-//                     topContent={
-//                         <Text style={styles.centerText}>
-//                             Go to <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on your computer and scan the QR code.
-//                         </Text>
-//                     }
-//                     bottomContent={
-//                         <TouchableOpacity style={styles.buttonTouchable}>
-//                             <Text style={styles.buttonText}>OK. Got it!</Text>
-//                         </TouchableOpacity>
-//                     }
-//                 />
-//             );
-//         }
-//         return (
-//             <View style={styles.viewContainer}>
-//                 {content}
-//             </View>
-//         );
-//     }
-// }
-//
-// const styles = StyleSheet.create({
-//     viewContainer: {
-//         flex: 1,
-//         justifyContent: 'center',
-//         alignItems: 'center'
-//     },
-//     centerText: {
-//         flex: 1,
-//         fontSize: 18,
-//         padding: 32,
-//         color: '#777',
-//     },
-//     textBold: {
-//         fontWeight: '500',
-//         color: '#000',
-//     },
-//     buttonText: {
-//         fontSize: 21,
-//         color: 'rgb(0,122,255)',
-//     },
-//     buttonTouchable: {
-//         padding: 16,
-//     },
-// });
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+    Button
+} from 'react-native';
+import QRCodeScanner from 'react-native-qrcode-scanner';
+
+export default class QrScanner extends Component {
+    state = {
+        mode: 'view'
+    };
+
+    onCloseQRcode = (e) => {
+        this.setState({
+            mode: 'view'
+        });
+       if (e.data != null) {
+           alert('this data => ' + e.data);
+       }
+    };
+
+    onOpenQRcode = () => {
+        this.setState({
+            mode: 'qr-code'
+        });
+    };
+
+    render() {
+        let content = (
+            <Button
+                onPress={this.onOpenQRcode}
+                title="Scan QR-Code"
+                color="#841584">
+            </Button>
+        );
+
+        if (this.state.mode === 'qr-code') {
+            content = (
+                <QRCodeScanner
+                    onRead={this.onCloseQRcode}
+                    topContent={
+                        <Text style={styles.centerText}>
+                            Go to <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on your computer and scan the QR code.
+                        </Text>
+                    }
+                    bottomContent={
+                        <TouchableOpacity style={styles.buttonTouchable} onPress={this.onCloseQRcode}>
+                            <Text style={styles.buttonText}>OK. Got it!</Text>
+                        </TouchableOpacity>
+                    }
+                />
+            );
+        }
+        return (
+            <View style={styles.viewContainer}>
+                {content}
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    viewContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    centerText: {
+        flex: 1,
+        fontSize: 18,
+        padding: 32,
+        color: '#777',
+    },
+    textBold: {
+        fontWeight: '500',
+        color: '#000',
+    },
+    buttonText: {
+        fontSize: 21,
+        color: 'rgb(0,122,255)',
+    },
+    buttonTouchable: {
+        padding: 16,
+    },
+});
